42e84032696cf75150a49c465fa992652c177d55
Sathwik Pochampally <sathwik@outlook.com>
2017-10-15 20:59:58 -0500

    Added ability to print to interpreter and JSON file

--- twitter.py
+++ twitter.py
@@ -1,5 +1,6 @@
 import tweepy
 from tweepy import OAuthHandler
+import json
  
 consumer_key = 'YOUR-CONSUMER-KEY'
 consumer_secret = 'YOUR-CONSUMER-SECRET'
@@ -11,9 +12,18 @@ auth.set_access_token(access_token, access_secret)
  
 api = tweepy.API(auth)
 
+# Currently gives us a dump of raw meta data that can be further processed
 def process_or_store(tweet):
     print(json.dumps(tweet))
-    
+
 for status in tweepy.Cursor(api.home_timeline).items(10):
-    # Process a single status
-    process_or_store(status._json)
+    data = json.dumps(status._json)
+
+# # Dumps text from first 10 statuses to interpreter
+#  for status in tweepy.Cursor(api.home_timeline).items(10):
+#     # Process a single status
+#     print(status.text)
+
+# Prints to a JSON file
+with open('twitter.json', 'w') as outfile:
+    json.dump(data, outfile)
